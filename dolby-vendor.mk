354977a5b5037336465c825d418b0dc5b7db26a8
Dmitry <42121541+dkpost3@users.noreply.github.com>
2025-04-15 04:38:42 +0300

    update vendor; rm dolby vision

--- dolby-vendor.mk
+++ dolby-vendor.mk
@@ -6,18 +6,10 @@ PRODUCT_SOONG_NAMESPACES += \
     vendor/xiaomi/dolby
 
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/dolby/proprietary/vendor/etc/init/dolbycodec2.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/dolbycodec2.rc \
     vendor/xiaomi/dolby/proprietary/vendor/etc/init/vendor.dolby.hardware.dms@2.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.hardware.dms@2.0-service.rc \
-    vendor/xiaomi/dolby/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc \
+    vendor/xiaomi/dolby/proprietary/vendor/etc/init/vendor.dolby.media.c2@1.0-service.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/vendor.dolby.media.c2@1.0-service.rc
 
 PRODUCT_PACKAGES += \
-    c2.dolby.client \
-    c2.dolby.hevc.dec \
-    c2.dolby.hevc.enc \
-    c2.dolby.hevc.sec.dec \
-    c2.dolby.store \
-    libDecoderProcessor \
-    libEncoderProcessor \
     libcodec2_soft_ac4dec \
     libcodec2_soft_ddpdec \
     libcodec2_soft_dolby \
@@ -26,13 +18,7 @@ PRODUCT_PACKAGES += \
     libdeccfg \
     libdlbdsservice \
     libdlbpreg \
-    libdolbyottcameracontrol \
-    libeglcore \
-    libmisfpsop \
-    libmisoundsa \
-    libmispowerop \
     libspatializerparamstorage \
-    libswspatializer_ext \
     libdlbvol \
     libswdap \
     libswgamedap \
@@ -40,6 +26,5 @@ PRODUCT_PACKAGES += \
     libswvqe \
     vendor.dolby.hardware.dms@2.0-impl \
     vendor.dolby.hardware.dms@2.0 \
-    dolbycodec2 \
     vendor.dolby.hardware.dms@2.0-service \
     vendor.dolby.media.c2@1.0-service
